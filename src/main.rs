0d6b1ca79a58e55f0d4cbfa197af173333c7ee6d
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-06-06 17:54:38 -0700

    Add main.rs file entry point with arguments and auto file naming

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::fs::File;
+use std::env;
+use std::io::prelude::*;
+
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    
+    let seconds: u64 = args[1].parse().unwrap();
+    let frequency: f64 = args[2].parse().unwrap();
+    
+    let filename = format!("{}sec {}Hz sin.wav", seconds, frequency);
+    let mut file = File::create(filename)?;
+    let bytes = wav_file_generator::build_wav(seconds, frequency);
+    file.write_all(&bytes)?;
+    Ok(())
+}
